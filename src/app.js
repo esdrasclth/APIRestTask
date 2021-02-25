22f78e3191f89a485d7fa7940cd6edf21b7e21f7
Esdras Clother <esdras.clother@outlook.com>
2021-02-25 14:10:22 -0600

    Avanzando en la creacion de la api, ya podemos guardar datos atrvez del metodo POST

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ import TasksRoutes from './routes/tasks.routes'
 
 const app = express()
 
+app.use(express.json());
+
+// Setings
 app.set('port', process.env.PORT || 3000);
 
+// Routes
 app.get('/',  (req, res) => {
     res.json({message: 'Welcome to my application'})
 })
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,16 @@
 import {Router} from 'express'
+import Task from '../models/Task'
 
 const router = Router()
 
 router.get('/', (req, res) => {
     res.send('Tasks')
+})
+
+router.post('/', async (req, res) => {
+    const newTask = new Task({title: req.body.title, description: req.body.description});
+    const taskSave = await newTask.save();
+    res.json(taskSave)
 })
 
 export default router;
