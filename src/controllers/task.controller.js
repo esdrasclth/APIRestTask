dea275132bd9793ce52d1d6b78d9a00e6b8d41b8
Esdras Clother <esdras.clother@outlook.com>
2021-02-25 17:54:05 -0600

    codigo base de la api completado, ahun faltan modificaciones

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -24,3 +24,13 @@ export const deleteTask = async (req, res) => {
     const data = await Task.findByIdAndDelete(req.params.id);
     res.json({message: "Task were deleted successfully"});
 }
+
+export const findAllDoneTasks = async (req, res) => {
+    const tasks = await Task.find({done: true})
+    res.json(tasks)
+}
+
+export const updateTask =  async (req, res) => {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.json({message: "Task was update successfully"});
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -4,8 +4,15 @@ import * as taskCtrl from '../controllers/task.controller'
 const router = Router()
 
 router.get('/', taskCtrl.findAllTasks);
-router.get('/:id', taskCtrl.findOneTask);
 router.post('/', taskCtrl.createTask);
+
+router.get('/done', taskCtrl.findAllDoneTasks);
+
+router.get('/:id', taskCtrl.findOneTask);
 router.delete('/:id', taskCtrl.deleteTask);
+
+router.put('/:id', taskCtrl.updateTask);
+
+
 
 export default router;
